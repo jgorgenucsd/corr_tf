7ad5848010860a00b3b1d260eb45ea9f665d17d8
jgorgen <justin.gorgen@navy.mil>
2017-07-18 15:53:39 -0700

    Fixed error with b_grad collisions by splitting gradient calculation into two steps

--- correlation_grad_gpu.cc.cu
+++ correlation_grad_gpu.cc.cu
@@ -25,19 +25,6 @@ __global__ void CorrelationGradKernel(const float* a, const float*b,const float*
     int out1 = num_offsets;
     int out2 = num_cols * out1;
     int out3 = num_rows * out2;
-    /*
-    // Zero-initialize the output gradient
-    for (int i = 0; i < batch_size; i++) {
-        for (int j = blockIdx.x * blockDim.x + threadIdx.x; j < num_rows; j += blockDim.x * gridDim.x) {
-          for (int k = 0; k < num_cols; k++) {
-              int a_root = three_d_size*i + two_d_size*j+one_d_size * k;
-              for (int m =0; m < depth; m++ ) {
-                    out_a[a_root+m] = 0;
-                    out_b[a_root+m] = 0;
-              }
-          }
-        }
-    } */
 
     for (int i = blockIdx.z * blockDim.z; i < batch_size; i+= blockDim.z * gridDim.z) {
         int b_root = i*three_d_size;
@@ -73,6 +60,7 @@ __global__ void CorrelationGradKernel(const float* a, const float*b,const float*
                     int b_index = b_root + two_d_size*b_j + one_d_size * b_k +m;
                     float current_coefficient = grad[grad_index]/ depth;
                     out_a[a_index]+= current_coefficient*b[b_index];
+                    // THIS will clobber out_b occasionally, as different threads will try to write to the same b_index
                     out_b[b_index]+= current_coefficient*a[a_index];
 	    
                 }
@@ -82,6 +70,109 @@ __global__ void CorrelationGradKernel(const float* a, const float*b,const float*
         }
       }
 
+}
+
+__global__ void CorrelationGradAKernel(const float* a, const float*b,const float*grad,float* out_a,float*out_b, const int batch_size,const int num_rows, const int num_cols, const int depth,const int num_offsets, const int* offset_list)  {
+    int one_d_size   = depth;
+    int two_d_size   = one_d_size*num_cols;
+    int three_d_size = two_d_size*num_rows;
+
+    int out1 = num_offsets;
+    int out2 = num_cols * out1;
+    int out3 = num_rows * out2;
+
+    for (int i = blockIdx.z * blockDim.z; i < batch_size; i+= blockDim.z * gridDim.z) {
+        int b_root = i*three_d_size;
+        for (int j = blockIdx.x * blockDim.x + threadIdx.x; j < num_rows; j += blockDim.x * gridDim.x) {
+          for (int k = blockIdx.y*blockDim.y + threadIdx.y; k < num_cols; k += blockDim.y * gridDim.y) {
+            int grad_root = out3*i + out2*j+out1*k;
+            int a_root = three_d_size*i + two_d_size*j+one_d_size * k;
+            for( int m = 0 ; m < depth; m++) {
+              int a_index = a_root+m;
+              for (int l =0; l < num_offsets; l++ ) {
+                int j_offset = offset_list[2*l];
+                int k_offset = offset_list[2*l+1];
+                int min_j = 0;
+                int max_j = num_rows;
+                int min_k = 0;
+                int max_k = num_cols;
+                if(j_offset < 0){
+                    min_j = -1*j_offset;
+                }else{
+                    max_j -= j_offset;
+                }
+                if(k_offset < 0){
+                    min_k = -1*k_offset;
+                }else{
+                    max_k -= k_offset;
+                }
+
+                int grad_index =  grad_root+ l;
+                if( j >= min_j && j < max_j  && k >= min_k && k < max_k)
+                {
+                    int b_j = j+j_offset;
+                    int b_k = k+k_offset;
+                    int b_index = b_root + two_d_size*b_j + one_d_size * b_k +m;
+                    float current_coefficient = grad[grad_index]/ depth;
+                    out_a[a_index]+= current_coefficient*b[b_index];
+                }
+              }
+             }
+           }
+        }
+      }
+
+}
+
+__global__ void CorrelationGradBKernel(const float* a, const float*b,const float*grad,float* out_a,float*out_b, const int batch_size,const int num_rows, const int num_cols, const int depth,const int num_offsets, const int* offset_list)  {
+    int one_d_size   = depth;
+    int two_d_size   = one_d_size*num_cols;
+    int three_d_size = two_d_size*num_rows;
+
+    int out1 = num_offsets;
+    int out2 = num_cols * out1;
+    int out3 = num_rows * out2;
+
+    for (int i = blockIdx.z * blockDim.z; i < batch_size; i+= blockDim.z * gridDim.z) {
+        for (int b_j = blockIdx.x * blockDim.x + threadIdx.x; b_j < num_rows; b_j += blockDim.x * gridDim.x) {
+          for (int b_k = blockIdx.y*blockDim.y + threadIdx.y; b_k < num_cols; b_k += blockDim.y * gridDim.y) {
+            int b_root = i*three_d_size + two_d_size*b_j + one_d_size * b_k;
+            for( int m = 0 ; m < depth; m++) {
+              int b_index = b_root +m;
+              for (int l =0; l < num_offsets; l++ ) {
+                int j_offset = offset_list[2*l];
+                int k_offset = offset_list[2*l+1];
+                int j = b_j - j_offset;
+                int k = b_k - k_offset;
+                int min_j = 0;
+                int max_j = num_rows;
+                int min_k = 0;
+                int max_k = num_cols;
+                if(j_offset < 0){
+                    min_j = -1*j_offset;
+                }else{
+                    max_j -= j_offset;
+                }
+                if(k_offset < 0){
+                    min_k = -1*k_offset;
+                }else{
+                    max_k -= k_offset;
+                }
+                if( j >= min_j && j < max_j  && k >= min_k && k < max_k)
+                {
+                    int grad_root = out3*i + out2*j+out1*k;
+                    int grad_index =  grad_root+ l;
+                    int a_root = three_d_size*i + two_d_size*j+one_d_size * k;
+                    int a_index = a_root+m;
+                    float current_coefficient = grad[grad_index]/ depth;
+                    out_b[b_index]+= current_coefficient*a[a_index];
+	    
+                }
+              }
+             }
+           }
+        }
+      }
 
 }
 
@@ -113,7 +204,9 @@ void CorrelationGradKernelLauncher(const float* a, const float*b, const float*gr
   dim3 blocks(nx,ny,nz);
   dim3 threadsPerBlock(mx,my,mz);
   // Call the CUDA Kernel
-  CorrelationGradKernel<<<blocks, threadsPerBlock>>>(a, b, grad, out_a,out_b,batch_size,num_rows,num_cols,depth,num_offsets,offset_array);
+  // Calculate gradient A and gradient B separately to avoid collisions 
+  CorrelationGradAKernel<<<blocks, threadsPerBlock>>>(a, b, grad, out_a,out_b,batch_size,num_rows,num_cols,depth,num_offsets,offset_array);
+  CorrelationGradBKernel<<<blocks, threadsPerBlock>>>(a, b, grad, out_a,out_b,batch_size,num_rows,num_cols,depth,num_offsets,offset_array);
 }
 
 #endif
